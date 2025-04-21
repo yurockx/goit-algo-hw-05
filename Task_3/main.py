2c888104a931ced7946fb84007251194c127d28e
yurockx <71849449+yurockx@users.noreply.github.com>
2025-04-21 23:34:11 +0300

    small_adjust2

--- Task_3/main.py
+++ Task_3/main.py
@@ -1,10 +1,9 @@
 import sys
 import logs_check
-from pathlib import Path
 from typing import Optional
 
 
-def run(file_path, e_level=None):
+def run(file_path: str, e_level: Optional[str] = None):
     try:
         logs = logs_check.load_logs(file_path)
         parsed_logs = logs_check.parse_log_line(logs)
@@ -13,6 +12,7 @@ def run(file_path, e_level=None):
     except Exception as e:
         print(f"Error: {e}")
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 3:
         run(sys.argv[1], sys.argv[2])
@@ -20,5 +20,5 @@ if __name__ == "__main__":
         run(sys.argv[1])
     else:
         print("Only two commands can be used! path/to/file and errorlevel")
-        print("Use correct commands: python main.py <directory/path> ->with no spaces otherwise add quotes")
-        print("Or add an error level to see mor details like: <directory/path>space>errorlevel")
+        print("python main.py <directory/path>")
+        print("Or python main.py <directory/path> errorlevel")
